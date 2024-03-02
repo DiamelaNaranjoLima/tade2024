4fe13924eb970bebaa70dac4ebdbcf2aa7154f24
DiamelaNaranjoLima <159169430+DiamelaNaranjoLima@users.noreply.github.com>
2024-03-03 00:13:13 +0100

    Add files via upload

---
+++ AND/EJAND.vhd
@@ -0,0 +1,14 @@
+library IEEE;
+	use IEEE.std_logic_1164.all;
+entity exampleAnd is
+	port(
+		A,B : in std_logic; -- Entradas
+		C: out std_logic -- Salida
+	);
+end exampleAnd;
+
+architecture archAnd of exampleAnd is
+
+begin
+	C <= A and B; -- Compuerta AND
+end architecture archAnd;
